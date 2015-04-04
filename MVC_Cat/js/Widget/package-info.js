e8d3b0153291ee3360f628331b7f2f140f4c4657
Hoody123 <Hoody_gs@qq.com>
2015-04-04 22:32:16 +0800

    大量的更新
    resetpassword的初步完成
    Object.package的生成及绑定

--- MVC_Cat/js/Widget/package-info.js
+++ MVC_Cat/js/Widget/package-info.js
@@ -38,3 +38,46 @@ MPWidget.PackageInfo.New=function(packageDetail)
 
     return content;
 }
+
+MPWidget.PackageInfo.Bind = function () {
+    $(document).on("click", ".widget-packageinfo .edit", edit_click)
+
+    .on("click", ".widget-packageinfo .follow", follow_click)
+
+    .on("click", ".widget-packageinfo .unfollow", unfollow_click);
+
+    function edit_click() {
+        //提取id
+        var id = $(this).attr("data-id");
+        location.href = "/package/" + id + "/edit";
+        //跳转到编辑页
+    }
+
+    function follow_click() {
+        var t = $(this);
+        var id = t.attr("data-id");
+        MPObject.Package.Follow(id, function () {
+            t.removeClass("follow");
+            t.addClass("unfollow");
+            t.text("已关注");
+            MPMessageBox.New(MPMessageBox.Icons.OK, "关注成功!");
+        })
+    }//关注操作
+
+    function unfollow_hover() {
+        $(this).text("取消关注");
+    }//鼠标指到"已关注"按钮上,文本变成"取消关注"
+
+    function unfollow_click() {
+        var t = $(this);
+        var box = MPMessageBox.New(MPMessageBox.Icons.Warn, "确认要取消关注?");
+        box.onOK = function myfunction() {
+            var id = t.attr("data-id");
+            MPObject.Package.UnFollow(id, function () {
+                t.removeClass("unfollow");
+                t.addClass("follow");
+                t.text("关注");
+            })
+        }
+    }//取消关注
+}
